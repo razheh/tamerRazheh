5b751e9684e6568dd7358eeb7feabd67950489e0
razheh <77445354+razheh@users.noreply.github.com>
2023-01-09 17:38:12 +0000

    Add files via upload

---
+++ profile/forms/contact.php
@@ -0,0 +1,37 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  
+  $receiving_email_address = 'tamer.razheh@yahoo.com';
+  $from_email = 'info@xtrailhosting.com';
+  //$from_email = 'info@xtrailhosting.com';
+  
+
+  
+  $to = $receiving_email_address;
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $message1=
+						"Full Name : ".$name."\n".
+						"Email : ".$email ."\n".
+						"Subject : ".$subject ."\n".
+						"Message : ".$message."\n";
+
+        
+        $from="From: $name<$from_email>\r\nReturn-path: $email";
+		$subject="Enquiry from site";
+			mail($to, $subject, $message1, $from);
+        if ($mail) {
+                echo 'OK';
+            }
+         else {
+            echo '<div class="sent-message"></div>';
+        }
+?>
